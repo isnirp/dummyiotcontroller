1a578ae6a80c251ee504c02b857b0e61e4d2acd4
Prince Amankwah <p.amankwah@livello.com>
2021-06-02 10:54:33 +0200

    comm: refactor communication class

--- src/main/java/com/flimbis/App.java
+++ src/main/java/com/flimbis/App.java
@@ -1,13 +1,27 @@
 package com.flimbis;
 
+import io.reactivex.rxjava3.core.*;
+
 /**
  * Hello world!
  *
  */
 public class App 
 {
+    private static MqttCommunication mqttClient;
+
+    public App(){}
+
     public static void main( String[] args )
     {
+        mqttClient = new MqttCommunication("localhost",1884);
+        mqttClient.connect();
+        
         System.out.println( "Hello World!" );
+        // subscribe to topic
+        mqttClient.subTopic("/dummy/temp", 1);
+        // consume temperature data
+        //      publish to iot core
+        Observable<String> observable = Observable.just("Hello");
     }
 }
